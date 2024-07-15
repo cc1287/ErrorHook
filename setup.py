a43fd02620ecad3a0a7f57691c2a50d644b515b9
cc1287 <cc1287@yeah.net>
2024-07-15 17:12:28 +0800

    2.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setuptools.setup(
     name="ErrorHook",
-    version="2.1.0",
+    version="2.1.1",
     author="cc1287",
     author_email="bilibili_cc1287@126.com",
     description="Hook your Error.",
