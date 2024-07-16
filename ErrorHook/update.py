b72d19a32a439805cca191d3baf1246074e84283
cc1287 <cc1287@yeah.net>
2024-07-16 10:03:22 +0800

    update.py

--- ErrorHook/update.py
+++ ErrorHook/update.py
@@ -1,1 +1,12 @@
 import requests
+import os,tempfile
+import subprocess
+
+with tempfile.TemporaryDirectory() as temp_dir:
+    print('临时目录:', temp_dir)
+    temp_file_path = os.path.join(temp_dir, 'setup.py')
+    with open(temp_file_path, 'w') as f:
+        f.write(requests.get('https://gitee.com/cc1287/error-hook/raw/master/setup.py').text)
+    print(subprocess.run('certutil -hashfile '+temp_file_path+' SHA256', shell=True,
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE, 
+                         universal_newlines=True).stdout)
